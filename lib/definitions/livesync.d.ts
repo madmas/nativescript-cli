e429bb947bdd1a9390c181f28d943f918c13ad6e
fatme <hfatme@gmail.com>
2018-08-23 10:19:51 +0300

    Fix "Cannot find connected devices" error when the env is not properly configured and "Sync to Playground" option is selected

--- lib/definitions/livesync.d.ts
+++ lib/definitions/livesync.d.ts
@@ -516,6 +516,7 @@ interface ILiveSyncCommandHelperAdditionalOptions extends IBuildPlatformAction, 
 	 * @returns {string} The build output directory.
 	 */
 	getOutputDirectory?(options: IOutputDirectoryOptions): string;
+	syncToPreviewApp?: boolean;
 }
 
 interface ILiveSyncCommandHelper {
@@ -534,7 +535,7 @@ interface ILiveSyncCommandHelper {
 	 * @param {string} platform The platform to be validated.
 	 * @return {Promise<void>}
 	 */
-	validatePlatform(platform: string): Promise<void>;
+	validatePlatform(platform: string): Promise<IDictionary<IValidateOutput>>;
 
 	/**
 	 * Executes livesync operation. Meant to be called from within a command.
